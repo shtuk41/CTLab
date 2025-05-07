78eb4561a6978e4e55baa0c400d4a17bd2a512d8
shtuk41 <shtuk41@gmail.com>
2025-05-06 21:18:15 -0500

    added couple spheres for testing and redefined voxel pitch to 100 micrometers

--- Model/include/modelDefs.h
+++ Model/include/modelDefs.h
@@ -4,13 +4,13 @@ const int nVoxelsX = 100;
 const int nVoxelsY = 100;
 const int nVoxelsZ = 100;
 
-const double xVoxelPitch = 10;
-const double yVoxelPitch = 10;
-const double zVoxelPitch = 10;
+const double xVoxelPitch = 0.1;
+const double yVoxelPitch = 0.1;
+const double zVoxelPitch = 0.1;
 
 const int nDetectorResY = 1736;
 const int nDetectorResZ = 1536;
 
-const double yDetectorPitch = 10;
-const double zDetectorPitch = 10;
+const double yDetectorPitch = 1;
+const double zDetectorPitch = 1;
 
